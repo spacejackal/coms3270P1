95868fda889ed8864afcc1c6db699c23400f3f1c
jacob leary <damnationmuffin@gmail.com>
2024-09-22 19:33:34 -0500

    added test file and changes to util.c

---
+++ TestUtil.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "util.h"
+
+int main() {
+	List li;
+	Listinit(*li, 5);
+	return 1;
+}
--- util.c
+++ util.c
@@ -4,17 +4,22 @@
 
 void *allocateArray(int rows, int columns) {
 	double* arr;
-	arr =calloc(sizeOf(double) *(row * columns));
+	arr =calloc(sizeOf(double) * (row * columns));
 }
 
 int ListInit(List* l, int max_elmt_size){
 	List l;
+	l->data = collac(max_elmt_size * 10);
+	l->max_element_size = max_elmt_size;
 	l->max_size = 10;
 	l->size = 0;
 	return 0;
 }
 
 void listAddEnd(List* l, void* elmt){
+	if (l->size != l->max_size) {
+		l->data + (l->max_element_size * l->size) = *elmt;
+	}
 }
 
 void* listGet(List* l, int index){

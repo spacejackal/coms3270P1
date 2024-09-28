2ab9261caae7d6775eeeccc61cf91dcb7ca6f54e
jacob leary <damnationmuffin@gmail.com>
2024-09-28 00:03:50 -0500

    changed data tyes for pcd_t

--- pointcloud.h
+++ pointcloud.h
@@ -5,11 +5,11 @@ int main();
 
 
 typedef struct pcd_t{		//this struct is used to keep track of points in a condensed and easy manner
-	double height, x, y;
-	double *north;
-	double *south;
-	double *east;
-	double *west;
+	double height, x, y , water_level;
+	pcd_t* north;
+	pcd_t* south;
+	pcd_t* east;
+	pcd_t* west;
 }pcd_t;
 
 void imagePointCloud(List* l,int width, char* filename);

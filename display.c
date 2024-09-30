7c3b54a1698068700f630cb4d500ce339a7f70bd
jacob leary <damnationmuffin@gmail.com>
2024-09-29 19:54:31 -0500

    updated display.c

--- display.c
+++ display.c
@@ -3,7 +3,22 @@
 #include "util.c"
 #include "pointcloud.h"
 #include "pointcloud.c"
+#include "bmp.h"
+#include "bmp.c"
 
 int main() {
+	List l;
+	List* pL = &l;
+	Stats s;
+	Stats* pS = &s;
+	pL->stats = pS;
+	char filename[] = "imagePointCloud.out";
 
+	ListInit(pL, sizeof(pcd_t));
+	int width = 0;
+	int* pWidth = &width;
+	readPointCloudData(stdin, pWidth, pL);
+	//stat1();
+	imagePointCloud(pL, width, filename);
+	return 1;
 }

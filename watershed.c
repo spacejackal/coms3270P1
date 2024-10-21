a10be85aee88c440508f707f70e9043a3645ca02
jacob leary <damnationmuffin@gmail.com>
2024-10-21 13:47:29 -0500

    other fixes

--- watershed.c
+++ watershed.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include "pointcloud.h"
 #include "util.h"
+#include "pointcloud.h"
 
 int main(int argc, char** argv) {
 	char* infile;
@@ -11,11 +11,11 @@ int main(int argc, char** argv) {
 	char* ofilebase;
 	int* seq;
 	
-	int sscanf(const char* infile, const int* iter, const float* iwater, const float* wcoef, const float* ecoef, const char* ofilebase, const int* seq);
+sscanf(const char* infile, const int* iter, const float* iwater, const float* wcoef, const float* ecoef, const char* ofilebase, const int* seq);
 pointcloud_t pc;
 pointcloud_t* pPC;
-pPC->wcoef = wcoef;
-pPC->ecoef = ecoef;
+pPC->wcoef = (double*)wcoef;
+pPC->ecoef = (double*)ecoef;
 FILE* pInFile = (FILE*)infile;
 pPC = readPointCloudData(pInFile);
 

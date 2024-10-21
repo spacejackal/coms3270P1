d69a8792a412fb566cf5351c235df722ac54f613
jacob leary <damnationmuffin@gmail.com>
2024-10-21 13:56:12 -0500

    trying to inichialise watershed.c

--- watershed.c
+++ watershed.c
@@ -2,21 +2,20 @@
 #include "util.h"
 #include "pointcloud.h"
 
-int main(int argc, char** argv) {
-	char* infile;
-	int* iter;
-	float* iwater;
-	float* wcoef;
-	float* ecoef;
-	char* ofilebase;
-	int* seq;
+int main(int argc, char* argv[]) {
+	char infile = argv[1];
+	int iter = argv[2];
+	float iwater = argv[3];
+	float wcoef = argv[4];
+	float ecoef = argv[5];
+	char ofilebase = argv[6];
+	int seq = argv[7];
 	
-sscanf( char* infile,  int* iter,  float* iwater, float* wcoef,  float* ecoef, char* ofilebase, int* seq);
 pointcloud_t pc;
 pointcloud_t* pPC;
 //pPC->wcoef = (double*)wcoef;
 //pPC->ecoef = (double*)ecoef;
-FILE* pInFile = (FILE*)infile;
+FILE* pInFile = fopen(infile,r);
 pPC = readPointCloudData(pInFile);
 
 

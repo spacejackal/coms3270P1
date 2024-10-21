5da0f276299e403682af34e997d267d38486fb44
jacob leary <damnationmuffin@gmail.com>
2024-10-21 16:20:54 -0500

    more test cases

--- watershed.c
+++ watershed.c
@@ -4,13 +4,19 @@
 #include "pointcloud.h"
 
 int main(int argc, char* argv[]) {
-	printf("we got to main\n");
+	printf("we got to main 1\n");
 	const char* __restrict infile = argv[1];
+	printf("we got to main 2\n");
 	int iter = atoi(argv[2]);
+	printf("we got to main 3\n");
 	float iwater = atof(argv[3]);
+	printf("we got to main 4\n");
 	float wcoef = atof(argv[4]);
+	printf("we got to main 5\n");
 	float ecoef = atof(argv[5]);
+	printf("we got to main 6\n");
 	const char* ofilebase = argv[6];
+	printf("we got to main 7\n");
 	int seq = atoi(argv[7]);
 	printf("we are defineing the PC\n");
 pointcloud_t pc;

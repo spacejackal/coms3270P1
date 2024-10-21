b6df5298e6c22208cd89bb0feab161b246ee12bd
jacob leary <damnationmuffin@gmail.com>
2024-10-21 13:44:29 -0500

    other water shed syntax fixes

--- watershed.c
+++ watershed.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 #include "pointcloud.h"
+#include "util.h"
 
 int main(int argc, char** argv) {
+	char* infile;
+	int* iter;
+	float* iwater;
+	float* wcoef;
+	float* ecoef;
+	char* ofilebase;
+	int* seq;
 	
 	int sscanf(const char* infile, const int* iter, const float* iwater, const float* wcoef, const float* ecoef, const char* ofilebase, const int* seq);
-	pointcloud_t pc;
-	pointcloud_t* pPC;
-	pPC->wcoef = wcoef;
-	pPC->ecoef = ecoef;
-	FILE* pInFile = (FILE*)infile;
-	pPC = readPointCloudData(pInFile);
+pointcloud_t pc;
+pointcloud_t* pPC;
+pPC->wcoef = wcoef;
+pPC->ecoef = ecoef;
+FILE* pInFile = (FILE*)infile;
+pPC = readPointCloudData(pInFile);
 
 
 	

fc549bb2cd35c0f8680a11c8455c828df28e713d
jacob leary <damnationmuffin@gmail.com>
2024-10-23 18:48:39 -0500

    added

--- pointcloud.c
+++ pointcloud.c
@@ -280,11 +280,8 @@ void watershedStep(pointcloud_t* pc) {
 
 	for (int i = 0; i < points->size; i++) {
 		pcd_t* p = listGet(pc->points, i);
-		if (p->wd += *temps[i] < 0) {
-			p->wd = 0;
-		}else{
+
 			p->wd += *temps[i];
-		}
 	}
 }
 

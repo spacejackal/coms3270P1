322b1a88a97492020fc5441cf6a2ed8d1ef098b4
jacob leary <damnationmuffin@gmail.com>
2024-10-23 16:30:09 -0500

    looks better

--- watershed.c
+++ watershed.c
@@ -34,20 +34,20 @@ pPC->points->stats->low = low;
 pPC->points->stats->high = high;
 
 watershedAddUniformWater(pPC, iwater);
-pcd_t* temp = listGet(pPC->points, 999);
+pcd_t* temp = listGet(pPC->points, 693);
 printf("other info Rx:%lf Ry:%lf x:%d y:%d height:%lf \n", temp->x, temp->y, temp->relitiveX, temp->relitiveY, temp->height);
 //pcd_t* north = temp->north;
 pcd_t* east = temp->east;
-//pcd_t* west = temp->west;
+pcd_t* west = temp->west;
 pcd_t* south = temp->south;
-//printf("NORTH info Rx:%lf Ry:%lf x:%d y:%d height:%lf \n", north->x, north->y, north->relitiveX, north->relitiveY, north->height);
 watershedStep(pPC);
-//printf("WEST info Rx:%lf Ry:%lf x:%d y:%d height:%lf \n", west->x, west->y, west->relitiveX, west->relitiveY, west->height);
+printf("NORTH info Rx:%lf Ry:%lf x:%d y:%d height:%lf \n", north->x, north->y, north->relitiveX, north->relitiveY, north->height);
+printf("SOUTH info Rx:%lf Ry:%lf x:%d y:%d height:%lf \n", south->x, south->y, south->relitiveX, south->relitiveY, south->height);
+printf("EAST info Rx:%lf Ry:%lf x:%d y:%d height:%lf \n", east->x, east->y, east->relitiveX, east->relitiveY, east->height);
+printf("WEST info Rx:%lf Ry:%lf x:%d y:%d height:%lf \n", west->x, west->y, west->relitiveX, west->relitiveY, west->height);
 
 //pcd_t* north = temp->north;
-printf("EAST info Rx:%lf Ry:%lf x:%d y:%d height:%lf \n", east->x, east->y, east->relitiveX, east->relitiveY, east->height);
 
-printf("SOUTH info Rx:%lf Ry:%lf x:%d y:%d height:%lf \n", south->x, south->y, south->relitiveX, south->relitiveY, south->height);
 
 
 //watershedStep(pPC);

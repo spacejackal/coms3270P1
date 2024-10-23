6398fe7ae6b72ccdb1e03bea73ec4a9bf6652073
jacob leary <damnationmuffin@gmail.com>
2024-10-23 18:19:16 -0500

    added comments

--- watershed.c
+++ watershed.c
@@ -35,16 +35,18 @@ pPC->points->stats->high = high;
 
 watershedAddUniformWater(pPC, iwater);
 pcd_t* temp = listGet(pPC->points, 4693);
-printf("other info Rx:%lf Ry:%lf row:%d col:%d height:%lf \n", temp->x, temp->y, temp->relitiveX, temp->relitiveY, temp->height);
+printf("other info Rx:%lf Ry:%lf row:%d col:%d height:%lf water:%lf \n", temp->x, temp->y, temp->relitiveX, temp->relitiveY, temp->height,temp->wd);
 
-pcd_t* north = temp->north;
-pcd_t* east = temp->east;
-pcd_t* west = temp->west;
-pcd_t* south = temp->south;
-printf("NORTH info Rx:%lf Ry:%lf row:%d col:%d height:%lf \n", north->x, north->y, north->relitiveX, north->relitiveY, north->height);
-printf("SOUTH info Rx:%lf Ry:%lf row:%d col:%d height:%lf \n", south->x, south->y, south->relitiveX, south->relitiveY, south->height);
-printf("EAST info Rx:%lf Ry:%lf row:%d col:%d height:%lf \n", east->x, east->y, east->relitiveX, east->relitiveY, east->height);
-printf("WEST info Rx:%lf Ry:%lf row:%d col:%d height:%lf \n", west->x, west->y, west->relitiveX, west->relitiveY, west->height);
+//pcd_t* north = temp->north;
+//pcd_t* east = temp->east;
+//pcd_t* west = temp->west;
+//pcd_t* south = temp->south;
+//printf("NORTH info Rx:%lf Ry:%lf row:%d col:%d height:%lf \n", north->x, north->y, north->relitiveX, north->relitiveY, north->height);
+//printf("SOUTH info Rx:%lf Ry:%lf row:%d col:%d height:%lf \n", south->x, south->y, south->relitiveX, south->relitiveY, south->height);
+//printf("EAST info Rx:%lf Ry:%lf row:%d col:%d height:%lf \n", east->x, east->y, east->relitiveX, east->relitiveY, east->height);
+//printf("WEST info Rx:%lf Ry:%lf row:%d col:%d height:%lf \n", west->x, west->y, west->relitiveX, west->relitiveY, west->height);
+
+
 watershedStep(pPC);
 
 //pcd_t* north = temp->north;

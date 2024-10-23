a3b616e2d7d1cc72186690e1e1761be1ae20a009
jacob leary <damnationmuffin@gmail.com>
2024-10-22 21:29:13 -0500

    otehr check

--- watershed.c
+++ watershed.c
@@ -40,8 +40,8 @@ initializeWatershed(pPC);
 //printf("the north pointt are at x %d, and low %d and height %lf\n", north->relitiveX, north->relitiveY, north->height);
 //pcd_t* south = temp->south;
 //printf("the south pointt are at x %d, and low %d and height %lf\n", south->relitiveX, south->relitiveY, south->height);
-temp = listGet(pPC->points, 0);
-printf("the first point is x:%lf y: %lf and height of %lf \n", temp->x, temp->y, temp->height);
+temp = listGet(pPC->points, 1);
+printf("the secod point is x:%lf y: %lf and height of %lf \n", temp->x, temp->y, temp->height);
 
 pPC->points->stats->low = low;
 imagePointCloud(pPC, "out");

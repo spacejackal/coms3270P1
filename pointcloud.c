16612f971692beeb6421098b06fdb0af8fd80e04
jacob leary <damnationmuffin@gmail.com>
2024-10-22 22:19:39 -0500

    trying something

--- pointcloud.c
+++ pointcloud.c
@@ -193,7 +193,7 @@ pointcloud_t* readPointCloudData(FILE* stream){
 	
 	pointcloud_t pc;
 	pointcloud_t* pPC = malloc(sizeof(pointcloud_t));
-	pPC->points = pTempList;
+	pPC->points = pL;
 	pPC->cols = width;
 	pPC->rows = (pL->size / width);
 	return pPC;

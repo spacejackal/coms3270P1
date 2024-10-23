98f33ffe281ad5d2ae64afd43fccfae40f117b9f
jacob leary <damnationmuffin@gmail.com>
2024-10-23 14:38:09 -0500

    errer looking

--- pointcloud.c
+++ pointcloud.c
@@ -262,7 +262,10 @@ void watershedStep(pointcloud_t* pc) {
 		}
 		temp -=  (p->wd * pc->ecoef);
 		temps[i] = &temp;
-		printf("the water depth is: %lf and the new offset is: %lf \n", p->wd,temp);
+		if (temp < -20 || temp> 20) {
+			printf("the x: %d and y:%d and the new offset is: %lf \n", p->relitiveX,p->relitiveY, temp);
+
+		}
 	}
 
 	for (int i = 0; i < points->size; i++) {

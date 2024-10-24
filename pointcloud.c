c96c1aedab95f040a575b859ea1f9b4020a2f66f
jacob leary <damnationmuffin@gmail.com>
2024-10-23 23:18:32 -0500

    blue

--- pointcloud.c
+++ pointcloud.c
@@ -335,8 +335,8 @@ void imagePointCloudWater(pointcloud_t* pc, double maxwd, char* filename) {
 		tempwater /= temp;
 
 		if (pListTemp->wd >= maxwd) {
-			section =(unsigned int) 256;
-			section = (unsigned int)256 << 24;
+			section =(unsigned int) 255;
+			section = (unsigned int)255 << 24;
 		}
 		else {
 			section = (unsigned int)temp;

ed361364130e12eabb29644f66640ce80287e0cd
jacob leary <damnationmuffin@gmail.com>
2024-10-23 21:29:54 -0500

    asdf

--- pointcloud.c
+++ pointcloud.c
@@ -188,7 +188,7 @@ pointcloud_t* readPointCloudData(FILE* stream){
 	
 	pointcloud_t pc;
 	pointcloud_t* pPC = malloc(sizeof(pointcloud_t));
-	pPC->points = pTempList;
+	pPC->points = pPC;
 	pPC->cols = width;
 	pPC->rows = (pL->size / width);
 	return pPC;

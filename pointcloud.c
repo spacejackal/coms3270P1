015a064df845256265cb1ee02319921d1b273e78
jacob leary <damnationmuffin@gmail.com>
2024-10-23 23:05:39 -0500

    fixed and more testing

--- pointcloud.c
+++ pointcloud.c
@@ -289,7 +289,7 @@ void watershedStep(pointcloud_t* pc) {
 
 double helper(double wcoef,double t1, double t2, double w1, double w2) {
 	double temp;
-	printf("t1:%lf, t2:%lf, w1:%lf, w2:%lf \n", t1, t2, w1, w2);
+	//printf("t1:%lf, t2:%lf, w1:%lf, w2:%lf \n", t1, t2, w1, w2);
 	temp = (t2 + w2);
 	temp -= (t1 + w1);
 	temp *= wcoef;

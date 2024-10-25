eaa27c8d1c353b1bf16ccb2367893ea52ac696a8
jacob leary <damnationmuffin@gmail.com>
2024-10-24 21:13:39 -0500

    undo

--- watershed.c
+++ watershed.c
@@ -30,6 +30,8 @@ pPC->ecoef = tecoef;
 double low = pPC->points->stats->low;
 double high = pPC->points->stats->high;
 
+printf("the pc stats are low:%lf and high:%lf pre watershed \n", pPC->points->stats->low, pPC->points->stats->high);
+//imagePointCloudWater(pPC,wdmax, "preOut");
 
 initializeWatershed(pPC);
 
@@ -38,30 +40,41 @@ pPC->points->stats->high = high;
 
 watershedAddUniformWater(pPC, iwater);
 
+pcd_t* temp = listGet(pPC->points, 0);
+
+//printf("other info Rx:%lf Ry:%lf col:%d row:%d height:%lf water:%lf \n", temp->x, temp->y, temp->col, temp->row, temp->height,temp->wd);
+//imagePointCloudWater(pPC, wdmax, "outWater");
+pcd_t* north = temp->north;
+pcd_t* east = temp->east;
+pcd_t* west = temp->west;
+pcd_t* south = temp->south;
+//printf("NORTH info Rx:%lf Ry:%lf col:%d row:%d height:%lf \n", north->x, north->y, north->relitiveX, north->relitiveY, north->height);
+//printf("SOUTH info Rx:%lf Ry:%lf col:%d row:%d height:%lf \n", south->x, south->y, south->col, south->row, south->height);
+//printf("EAST info Rx:%lf Ry:%lf col:%d row:%d height:%lf \n", east->x, east->y, east->col, east->row, east->height);
+//printf("WEST info Rx:%lf Ry:%lf col:%d row:%d height:%lf \n", west->x, west->y, west->relitiveX, west->relitiveY, west->height);
+
+
+
+
+watershedStep(pPC);
 
 pPC->points->stats->low = low;
 pPC->points->stats->high = high;
 
+imagePointCloudWater(pPC,wdmax, ofilebase);
 int totalCount =0;
-int seqCount = 1;
-char num[5];
-watershedStep(pPC);
-imagePointCloudWater(pPC, wdmax, "out.gif");
-
+int seqCount = 0;
 //for (int i = 0; i < iter; i++) {
-//	watershedStep(pPC);
 //	if (seqCount == seq) {
-//		sprintf(num, "%d", totalCount);
 //		char ofile[20];
 //		strcpy(ofile, ofilebase);
-//		strcat(ofile, num);
-//		strcat(ofile, ".gif");
-//		imagePointCloudWater(pPC, wdmax, ofile);
+//		strcat(ofile, (itoa(totalCount)))
+//		imagePointCouldWater(pPC, wdmax, ofilebase);
 //		seqCount = 0;
 //		totalCount++;
-//		*num = "";
 //	}
 //	pPC->points->stats->low = low;
+//	watershedStep(pPC);
 //	seqCount++;
 //}
 

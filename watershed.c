2fcc44b4e1bf65d357ecc9257cff670904e92bfe
jacob leary <damnationmuffin@gmail.com>
2024-10-25 10:46:56 -0500

    more comments and got rid of unnedded comments

--- watershed.c
+++ watershed.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 
 int main(int argc, char* argv[]) {
+	// the below block of code reads the arguments that are given to watershed when it is run
 	const char* __restrict infile = argv[1];
 	int iter = atoi(argv[2]);
 	float iwater = atof(argv[3]);
@@ -14,80 +15,70 @@ int main(int argc, char* argv[]) {
 	float wdmax = atof(argv[7]);
 	int seq = atoi(argv[8]);
 
-pointcloud_t pc;
-pointcloud_t* pPC = (pointcloud_t*)malloc(sizeof(pointcloud_t));
-
-
-FILE* pInFile = fopen(infile,"r");
-
-
-pPC = readPointCloudData(pInFile);
-double twcoef = (double)wcoef;
-double tecoef = (double)ecoef;
-
-pPC->wcoef = twcoef;
-pPC->ecoef = tecoef;
-
-double low = pPC->points->stats->low;
-double high = pPC->points->stats->high;
-
-printf("the pc stats are low:%lf and high:%lf pre watershed \n", pPC->points->stats->low, pPC->points->stats->high);
-//imagePointCloudWater(pPC,wdmax, "preOut");
-
-initializeWatershed(pPC);
-
-pPC->points->stats->low = low;
-pPC->points->stats->high = high;
-
-watershedAddUniformWater(pPC, iwater);
-
-pcd_t* temp = listGet(pPC->points, 0);
-
-//printf("other info Rx:%lf Ry:%lf col:%d row:%d height:%lf water:%lf \n", temp->x, temp->y, temp->col, temp->row, temp->height,temp->wd);
-//imagePointCloudWater(pPC, wdmax, "outWater");
-pcd_t* north = temp->north;
-pcd_t* east = temp->east;
-pcd_t* west = temp->west;
-pcd_t* south = temp->south;
-//printf("NORTH info Rx:%lf Ry:%lf col:%d row:%d height:%lf \n", north->x, north->y, north->relitiveX, north->relitiveY, north->height);
-//printf("SOUTH info Rx:%lf Ry:%lf col:%d row:%d height:%lf \n", south->x, south->y, south->col, south->row, south->height);
-//printf("EAST info Rx:%lf Ry:%lf col:%d row:%d height:%lf \n", east->x, east->y, east->col, east->row, east->height);
-//printf("WEST info Rx:%lf Ry:%lf col:%d row:%d height:%lf \n", west->x, west->y, west->relitiveX, west->relitiveY, west->height);
-
-
-
-
-watershedStep(pPC);
-
-
-pPC->points->stats->low = low;
-pPC->points->stats->high = high;
-imagePointCloudWater(pPC, wdmax, ofilebase);
-
-int totalCount =0;
-int seqCount = 1;
-char num[64];
-char ofile[64];
-for (int i = 0; i < iter; i++) {
-	if (i % seq == 0) {
-		sprintf(num, "%d", i);
-		strcpy(ofile, ofilebase);
-		strcat(ofile, num);
-		strcat(ofile, ".gif");
-		pPC->points->stats->low = low;
-		pPC->points->stats->high = high;
-		
-
-		imagePointCloudWater(pPC, wdmax, ofile);
-		seqCount = 0;
-		totalCount++;
-	}
-	//pPC->points->stats->low = low;
+	//inishilizeing the point cloud that will be used
+	pointcloud_t pc;
+	pointcloud_t* pPC = (pointcloud_t*)malloc(sizeof(pointcloud_t));
+	
+	//opening the file so it is ready to read
+	FILE* pInFile = fopen(infile,"r");
+	
+	//reading the points from the file expeced and throwing in the wcoef and ecoef to the pointcloud
+	pPC = readPointCloudData(pInFile);
+	double twcoef = (double)wcoef;
+	double tecoef = (double)ecoef;
+	pPC->wcoef = twcoef;
+	pPC->ecoef = tecoef;
+	
+	//setting a local high and low as with repeaded calls to imagepointcloud these get lost and become 0
+	double low = pPC->points->stats->low;
+	double high = pPC->points->stats->high;
+	
+	//adding the points adjasit to each point
+	initializeWatershed(pPC);
+	
+	//reinitilizeing the high and low
+	pPC->points->stats->low = low;
+	pPC->points->stats->high = high;
+	
+	//adding water to each point
+	watershedAddUniformWater(pPC, iwater);
+	
+	
+	
+	
+	
+	
 	watershedStep(pPC);
-	seqCount++;
-}
-
-
-return 1;
+	
+	
+	pPC->points->stats->low = low;
+	pPC->points->stats->high = high;
+	imagePointCloudWater(pPC, wdmax, ofilebase);
+	
+	int totalCount =0;
+	int seqCount = 1;
+	char num[64];
+	char ofile[64];
+	for (int i = 0; i < iter; i++) {
+		if (i % seq == 0) {
+			sprintf(num, "%d", i);
+			strcpy(ofile, ofilebase);
+			strcat(ofile, num);
+			strcat(ofile, ".gif");
+			pPC->points->stats->low = low;
+			pPC->points->stats->high = high;
+			
+	
+			imagePointCloudWater(pPC, wdmax, ofile);
+			seqCount = 0;
+			totalCount++;
+		}
+		//pPC->points->stats->low = low;
+		watershedStep(pPC);
+		seqCount++;
+	}
+	
+	
+	return 1;
 
 }

aed909044ce29fa57010af8ec60bf84491e6b6a6
jacob leary <damnationmuffin@gmail.com>
2024-10-24 21:12:05 -0500

    asd

--- watershed.c
+++ watershed.c
@@ -45,22 +45,25 @@ pPC->points->stats->high = high;
 int totalCount =0;
 int seqCount = 1;
 char num[5];
-for (int i = 0; i < iter; i++) {
-	watershedStep(pPC);
-	if (seqCount == seq) {
-		sprintf(num, "%d", totalCount);
-		char ofile[20];
-		strcpy(ofile, ofilebase);
-		strcat(ofile, num);
-		strcat(ofile, ".gif");
-		imagePointCloudWater(pPC, wdmax, ofile);
-		seqCount = 0;
-		totalCount++;
-		*num = "";
-	}
-	pPC->points->stats->low = low;
-	seqCount++;
-}
+watershedStep(pPC);
+imagePointCloudWater(pPC, wdmax, "out.gif");
+
+//for (int i = 0; i < iter; i++) {
+//	watershedStep(pPC);
+//	if (seqCount == seq) {
+//		sprintf(num, "%d", totalCount);
+//		char ofile[20];
+//		strcpy(ofile, ofilebase);
+//		strcat(ofile, num);
+//		strcat(ofile, ".gif");
+//		imagePointCloudWater(pPC, wdmax, ofile);
+//		seqCount = 0;
+//		totalCount++;
+//		*num = "";
+//	}
+//	pPC->points->stats->low = low;
+//	seqCount++;
+//}
 
 
 return 1;
